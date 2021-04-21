7709ff4ee5b9df8aa437b60fb3686a49e4e1f13c
Mona Zheng <mona.a.zheng@gmail.com>
2021-04-21 18:06:03 -0400

    user can pick the first and last bell of the day

--- ClockTowerBell/ViewController.swift
+++ ClockTowerBell/ViewController.swift
@@ -8,15 +8,38 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+    // SET ON/OFF Button properties
     @IBOutlet weak var buONOFFBTN: UIButton!
     let yellowColor = UIColor.init(red: 255/255, green: 236/255, blue: 149/255, alpha: 1.0);
     let blueColor = UIColor.init(red: 40/255, green: 74/255, blue: 119/255, alpha: 1.0);
     
+    // VARIBALES FOR SETTING THE DAILY START AND END TIMES
+    var timePicker = UIPickerView()
+    @IBOutlet weak var firstBellText: UITextField!
+    @IBOutlet weak var lastBellText: UITextField!
+    var activeField: UITextField?
+    let amOrPm = ["AM", "PM"]
+    let hour = [1,2,3,4,5,6,7,8,9,10,11,12]
+    // first saved bell
+    var hourTime:Int!
+    var timeOfDay:String!
+    // last saved bell
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Set up for the daily start and end time picker view
+        // set VC as textfield's delegate
+        firstBellText.delegate = self
+        lastBellText.delegate = self
+        // set VC as picker view delegate
+        timePicker.delegate = self
+        timePicker.dataSource = self
+        
         // retrieve on/off state when app is reopened,
         // starts out false if it is the first time app is being used
         let defaults = UserDefaults.standard
@@ -34,10 +57,6 @@ class ViewController: UIViewController {
         buONOFFBTN.layer.cornerRadius = 15
         buONOFFBTN.layer.borderWidth = 1.5
         buONOFFBTN.layer.borderColor = UIColor.clear.cgColor
-        
-
-        
-        
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -127,4 +146,61 @@ class ViewController: UIViewController {
         center.removeAllPendingNotificationRequests()
     }
     
+    
+    // SET DAILY START AND END TIME
+    func createTimePicker(){
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // done button
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        
+        activeField?.inputAccessoryView = toolbar
+        activeField?.inputView = timePicker
+        
+    }
+
+    @objc func donePressed(){
+        self.view.endEditing(true)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        2
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 12
+        } else {
+            return 2
+        }
+    }
+    
+
+    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return String(hour[row])
+        } else {
+            return amOrPm[row]
+        }
+    }
+    
+    // show bell times and edit the textfield that was selected
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        activeField = textField
+        createTimePicker()
+        return true
+    }
+    
+    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("ROW", row, "Component", component)
+        if component == 0 {
+            hourTime = hour[row]
+        } else {
+            timeOfDay = amOrPm[row];
+        }
+        activeField?.text = "\(String(hourTime)) " + timeOfDay
+        
+    }
 }
