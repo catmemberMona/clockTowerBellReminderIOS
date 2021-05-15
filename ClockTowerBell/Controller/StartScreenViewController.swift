dbf9a97bf745432e52007a07ae1f3592906e5650
Mona Zheng <mona.a.zheng@gmail.com>
2021-05-14 22:17:15 -0400

    created start and instruction view controllers. View the start scene as first scene at launch

---
+++ ClockTowerBell/Controller/StartScreenViewController.swift
@@ -0,0 +1,36 @@
+//
+//  StartScreenViewController.swift
+//  ClockTowerBell
+//
+//  Created by mona zheng on 5/14/21.
+//
+
+import UIKit
+
+class StartScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buStartIntro(_ sender: UIButton) {
+       
+        
+    }
+    
+    @IBAction func buSkipIntro(_ sender: UIButton) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
